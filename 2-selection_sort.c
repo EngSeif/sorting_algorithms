e1968057e0cefbe73cc477d4e670a9dc14a74168
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-16 19:07:55 +0200

    Mandatort Sorting Algorthim | Selection Sort | Fix Errors V2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,11 +20,12 @@ void selection_sort(int *array, size_t size)
             if (array[min_idx] > array[j])
                 min_idx = j;
         }
-        tmp = array[i];
-        array[i] = array[min_idx];
-        array[min_idx] = tmp;
-
-        print_array(array, size);
+        if (min_idx != i)
+        {
+            tmp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = tmp;
+            print_array(array, size);
+        }
     }
-    
 }
