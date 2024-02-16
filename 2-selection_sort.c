7567ebffb5bf93100bea063dc37ae26a60a37713
EngSeif <eng.seifmohamed2004@gmail.com>
2024-02-16 19:10:04 +0200

    Mandatort Sorting Algorthim | Selection Sort | CodeBeauty Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[min_idx] > array[j])
-                min_idx = j;
-        }
-        if (min_idx != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min_idx] > array[j])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
